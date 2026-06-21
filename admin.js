8f4c0273d9d0bbdc63f5163d813b87442643478a
preciousphillimon <preciousphillimon1@gmail.com>
2026-06-21 02:51:55 +0200

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,38 +1,6 @@
 const SUPABASE_URL = "https://bhzbvuqylfzvcogspbix.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoemJ2dXF5bGZ6dmNvZ3NwYml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5ODQwOTcsImV4cCI6MjA5NzU2MDA5N30.NcJrl4DONWt_DQyWX_dR6IBt4pXz-10nqofHf1hMMAs";
 
-document.getElementById("projectForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    const project = {
-        title: title.value,
-        category: category.value,
-        image_url: image.value,
-        project_url: link.value
-    };
-
-    const res = await fetch(`${SUPABASE_URL}/rest/v1/projects`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            apikey: SUPABASE_KEY,
-            Authorization: `Bearer ${SUPABASE_KEY}`,
-            Prefer: "return=representation"
-        },
-        body: JSON.stringify(project)
-    });
-
-    if (res.ok) {
-        alert("Project added!");
-        document.getElementById("projectForm").reset();
-    } else {
-        alert("Error adding project");
-    }
-});
-
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
-
 const dropZone = document.getElementById("dropZone");
 const fileInput = document.getElementById("fileInput");
 const preview = document.getElementById("preview");
